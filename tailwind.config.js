9fe6a545a6da5f3620ec2c095ca4a195b330f022
ACR1209 <andrescoronel1209@gmail.com>
2023-05-21 19:37:13 -0500

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:{
+        "off-black": "#1F1F1F",
+        "gray-normal": "#D9D9D9",
+        "red-normal": "#D62424",
+        "blued-normal": "#344992"
+      }
     },
   },
   plugins: [],
