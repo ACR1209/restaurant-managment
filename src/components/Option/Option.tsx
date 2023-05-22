d178161a41739c1c685893e478dc894e61a68963
ACR1209 <andrescoronel1209@gmail.com>
2023-05-21 20:00:06 -0500

    Created options

---
+++ src/components/Option/Option.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+type OptionProps = {
+  to: string;
+  img: {
+    src: string;
+    alt: string;
+  };
+  name: string;
+};
+
+function Option({ to, img, name }: OptionProps) {
+  return (
+    <Link
+        href={to}
+        className="w-full p-10 flex flex-col items-center justify-center hover:scale-110 transition-all"
+    >
+      <Image className="object-cover rounded-xl w-full h-1/2" width={1000} height={1000} src={img.src} alt={img.alt} />
+      <h2 className="text-2xl font-bold">{name}</h2>
+
+    </Link>
+  );
+}
+
+export default Option;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,34 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar/Navbar";
+import Option from "@/components/Option/Option";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <>
-      <Navbar/>
-      <main></main>
-    </>
+    <div className="h-screen overflow-hidden">
+      <Navbar />
+      <main className={`${inter.className} flex h-full items-center justify-center`}>
+        <div className="grid grid-cols-3">
+
+          <Option
+            to="/materials"
+            img={{ src: "/vegetable.jpg", alt: "Vegetales varios" }}
+            name="Inventario de materia prima"
+          />
+          <Option
+            to="/dishes"
+            img={{ src: "/pizza.jpg", alt: "Vegetales varios" }}
+            name="Gestión de platos"
+          />
+          <Option
+            to="/materials"
+            img={{ src: "/coin.jpg", alt: "Vegetales varios" }}
+            name="Realizar orden de compra"
+          />
+        </div>
+      </main>
+    </div>
   );
 }
